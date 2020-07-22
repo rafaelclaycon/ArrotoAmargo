f8e51cac591e217d4ba842493cef99384fdb38b7
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-07-21 22:58:02 -0300

    Adiciona ordenação à lista de cervejas

---
+++ ArrotoAmargo/Modelos/CervejaListaViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CervejaListaViewModel.swift
+//  ArrotoAmargo
+//
+//  Created by Rafael Schmitt on 21/07/20.
+//
+
+import Combine
+
+class CervejaListaViewModel: ObservableObject {
+    @Published var cervejas = avaliacaoDados
+    
+    func inicializar() {
+        self.ordenarAlfabeticamentePeloNomeDaCerveja()
+    }
+    
+    func ordenarAlfabeticamentePeloNomeDaCerveja() {
+        cervejas.sort { $0.nomeCerveja.lowercased() < $1.nomeCerveja.lowercased() }
+    }
+    
+    func ordenarPorNota() {
+        cervejas.sort { $0.nota > $1.nota }
+    }
+    
+    func ordenarPorIBU() {
+        cervejas.sort { $0.ibu > $1.ibu }
+    }
+}
