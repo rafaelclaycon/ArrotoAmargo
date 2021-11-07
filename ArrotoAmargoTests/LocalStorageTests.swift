ab68af1b05d4d7c12d1cc9973d7a8eb3afdaea4e
Rafael Claycon Schmitt <rafael.schmitt-ext@viavarejo.com.br>
2021-11-07 14:23:34 -0300

    Add EstiloCerveja test

--- ArrotoAmargoTests/LocalStorageTests.swift
+++ ArrotoAmargoTests/LocalStorageTests.swift
@@ -15,10 +15,12 @@ class LocalStorageTests: XCTestCase {
 
     override func setUpWithError() throws {
         XCTAssertEqual(try storage.getCervejaCount(), 0)
+        XCTAssertEqual(try storage.getEstiloCount(), 0)
     }
 
     override func tearDownWithError() throws {
         XCTAssertNoThrow(try storage.deleteAllCervejas())
+        XCTAssertNoThrow(try storage.deleteAllEstilos())
     }
     
     // MARK: - Cervejas
@@ -33,12 +35,12 @@ class LocalStorageTests: XCTestCase {
     
     // MARK: - Estilos
     
-//    func test_insertEstilosIntoDB_shouldReturnCorrectCount() throws {
-//        let estilos = ArrotoAmargoService.getCervejas()
-//        for estilo in estilos {
-//            XCTAssertNoThrow(try storage.insert(estilo: estilo))
-//        }
-//        XCTAssertEqual(try storage.getAllEstilos().count, 13)
-//    }
+    func test_insertEstilosIntoDB_shouldReturnCorrectCount() throws {
+        let estilos = ArrotoAmargoService.getEstilos()
+        for estilo in estilos {
+            XCTAssertNoThrow(try storage.insert(estilo: estilo))
+        }
+        XCTAssertEqual(try storage.getAllEstilos().count, 7)
+    }
 
 }
